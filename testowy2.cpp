94f79389e7d617157373284ca3fface7e6fe7279
krzysztofwroniak <cr0wss@outlook.com>
2025-04-30 20:28:32 +0200

    coś nowego tak o

--- testowy2.cpp
+++ testowy2.cpp
@@ -4,5 +4,5 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         std::cout << i << std::endl;
     }
-    std::cout<<"Koniec"<<std::endl;
+    std::cout<<"Koniec tego programu"<<std::endl;
 }
