65a6d40b03ec07c1f16cff35a3f92b1a8e550107
krzysztofwroniak <cr0wss@outlook.com>
2025-04-30 20:49:18 +0200

    próba force push

--- testowy2.cpp
+++ testowy2.cpp
@@ -4,5 +4,5 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         std::cout << i << std::endl;
     }
-    std::cout<<"Koniec tego programu ale nie dokonczony, jednak cos dodałem 123"<<std::endl;
+    std::cout<<"Koniec tego programu ale nie dokonczony, jednak jest 123"<<std::endl;
 }
