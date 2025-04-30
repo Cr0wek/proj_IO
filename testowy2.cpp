626864b492d69832b870ab4563a5e81ed05d0681
krzysztofwroniak <cr0wss@outlook.com>
2025-04-30 20:42:22 +0200

    zmiana jednak nastała areczku

--- testowy2.cpp
+++ testowy2.cpp
@@ -4,5 +4,5 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         std::cout << i << std::endl;
     }
-    std::cout<<"Koniec tego programu ale nie dokonczony"<<std::endl;
+    std::cout<<"Koniec tego programu ale nie dokonczony, jednak cos dodałem"<<std::endl;
 }
