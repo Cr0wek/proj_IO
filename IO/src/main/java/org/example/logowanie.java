d017ad926ce9a05c2a816c83d9a4159cb340c617
krzysztofwroniak <cr0wss@outlook.com>
2025-05-29 12:13:44 +0200

    dodanie logowanie

---
+++ IO/src/main/java/org/example/logowanie.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class logowanie {
+    public static void main(String[] args) {
+        System.out.println("Ruletka time, albo sie logujesz albo system32 sie usuwa");
+    }}
