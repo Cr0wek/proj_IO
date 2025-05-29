821e707846c680f015e433d3e0fe190d3060695e
krzysztofwroniak <cr0wss@outlook.com>
2025-05-29 14:05:22 +0200

    dodanie logowanie

--- IO/src/main/java/org/example/logowanie.java
+++ IO/src/main/java/org/example/logowanie.java
@@ -3,4 +3,13 @@ package org.example;
 public class logowanie {
     public static void main(String[] args) {
         System.out.println("Ruletka time, albo sie logujesz albo system32 sie usuwa");
+        System.out.println("wel to cwel");
+        System.out.println("wig to git");
+        System.out.println("wtc 9/11");
+        System.out.println("wlo to dno");
+        System.out.println("wml przy tobie to chuj");
+        System.out.println("siusiak");
+        System.out.println("siusiak");
+
+
     }}
