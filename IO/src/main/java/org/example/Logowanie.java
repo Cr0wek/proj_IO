19a9bfd30e1456b630ec6020c98d83079d939368
krzysztofwroniak <cr0wss@outlook.com>
2025-05-29 14:41:50 +0200

    dodanie logowanie

--- IO/src/main/java/org/example/Logowanie.java
+++ IO/src/main/java/org/example/Logowanie.java
@@ -4,14 +4,13 @@ import java.util.logging.Logger;
 public class Logowanie {
     private static final Logger logger = Logger.getLogger(Logowanie.class.getName());
     public static void main(String[] args) {
-        logger.info("Ruletka time, albo sie logujesz albo system32 sie usuwa");
-        logger.info("wel to cwel");
-        logger.info("wig to git");
-        logger.info("wtc 9/11");
-        logger.info("wlo to dno");
-        logger.info("wml przy tobie to chuj");
-        logger.info("siusiak");
-        logger.info("siusiak");
-
-
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        logger.info("IOIOIOIIOIOIOIOIOIOI time");
     }}
