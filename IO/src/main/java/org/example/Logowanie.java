8e3de5de1ae9bb57f430ef6f548f068280d46039
krzysztofwroniak <cr0wss@outlook.com>
2025-05-29 15:11:40 +0200

    dodanie logowanie

--- IO/src/main/java/org/example/Logowanie.java
+++ IO/src/main/java/org/example/Logowanie.java
@@ -1,10 +1,10 @@
 package org.example;
-import java.util.logging.Logger;
+
 
 public class Logowanie {
-    private static final Logger logger = Logger.getLogger(Logowanie.class.getName());
+
     public static void main(String[] args) {
-        logger.info("IOIOIOIIOIOIOIOIOIOI time");
-        logger.info("IOIOIOIIOIOIOIOIOIOI time");
+        System.out.println("IOIOIOIIOIOIOIOIOIOI time");
+        System.out.println("IOIOIOIIOIOIOIOIOIOI time");
 
     }}
