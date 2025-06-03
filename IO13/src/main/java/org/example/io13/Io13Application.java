1503e92951331f6991d386668a3347fcd08e7568
krzysztofwroniak <cr0wss@outlook.com>
2025-06-03 08:57:53 +0200

    dodanie SpringBooty

---
+++ IO13/src/main/java/org/example/io13/Io13Application.java
@@ -0,0 +1,13 @@
+package org.example.io13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Io13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Io13Application.class, args);
+    }
+
+}
---
+++ IO13/src/test/java/org/example/io13/Io13ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.io13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Io13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
